e4590c2e666ef096a4868293e0c42807a68b7389
awlaska <sofiassantossousa@gmail.com>
2022-11-22 17:01:38 +0000

    parte do login

--- src/Conta.java
+++ src/Conta.java
@@ -1,11 +1,16 @@
-import Scanner;
-
+import java.util.Map;
 import java.util.Scanner;
 
 public class Conta {
     //TODO 1. Um utilizador pode criar uma conta. Para aceder ao sistema, o utilizador deverá efetuar login.
+    private Map<String , String> user;
     public void login(){
+        Scanner usernamei = new Scanner(System.in);
+        Scanner passwordi = new Scanner(System.in);
         System.out.println("Introduza o seu username: ");
-        Scanner username = new Scanner(String);
+        String u = usernamei.nextLine();
+        System.out.println("Introduza a sua password: ");
+        String p = passwordi.nextLine();
+        user.put(u, p);
     }
 }
