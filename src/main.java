48fbc83b9e56f75f2422e80b1e7f41c014e70f11
AlexandrePSantos <69675582+AlexandrePSantos@users.noreply.github.com>
2022-11-29 17:32:45 +0000

    mais classes

--- src/main.java
+++ src/main.java
@@ -1,5 +1,8 @@
 public class main {
     public static void main(String[] args) {
-
+        System.out.println("Ola :p");
+        for(int i = 0; i>-1; i++){
+            System.out.println("<3");
+        }
     }
 }
