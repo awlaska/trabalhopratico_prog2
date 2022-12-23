e8f3661e2f2a1be7e69e60b23846d7c5eb71642d
AlexandrePSantos <69675582+AlexandrePSantos@users.noreply.github.com>
2022-12-23 16:48:21 +0000

    smt

--- src/Admin.java
+++ src/Admin.java
@@ -15,7 +15,8 @@ public class Admin extends Utilizador implements IListar{
          */
     public void menu(){
         Scanner input = new Scanner(System.in);
-        System.out.print("0 - Sair || 1 - LogOut || 2 - Gerir Users || 3 - Gerir Registos");
+        System.out.println("0 - Sair || 1 - LogOut || 2 - Gerir Users || 3 - Gerir Registos");
+        System.out.print(">> ");
         int op = input.nextInt();
         switch (op) {
             case 0:
@@ -23,10 +24,35 @@ public class Admin extends Utilizador implements IListar{
         }
     }
 
-    public void alterarTipoUser(){
-
+    public void gerirUser(){
+        Scanner input = new Scanner(System.in);
+        System.out.print("0 - Sair || 1 - Retoceder || 2 - Alterar tipo de user || 3 - Apagar User \n>> ");
+        int op = input.nextInt();
+        switch (op){
+            case 0:
+                return;
+            case 1:
+                break;
+            case 2:
+                alterarTipoUser();
+            case 3:
+                apagarUser();
+        }
     }
-    public void apagarUser(){}
+    public void alterarTipoUser(){
+        listarUser();
+        System.out.println("!!Alteração do tipo de user!!");
+        System.out.print("ID do user a alterar: \n>> ");
+        //Logic goes here
+        System.out.print("\nTipo de user a atribuir (ADMIN, DONO, CLIENTE): \n>> ");
+        //Logic goes here
+    }
+    public void apagarUser(){
+        listarUser();
+        System.out.println("!!Apagar user!!");
+        System.out.print("ID do user a apagar: \n>> ");
+        //Logic goes here
+    }
 
     //TODO listagens:
         /*
